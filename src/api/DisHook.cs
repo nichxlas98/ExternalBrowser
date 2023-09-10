7287828fbc85bc3bd69a202487fff8e0ef6d9bae
nichxlas98 <djflames50@gmail.com>
2023-09-09 22:41:26 -0500

    Added crediting, and did some minor cleanup

--- src/api/DisHook.cs
+++ src/api/DisHook.cs
@@ -3,14 +3,14 @@ using System.Collections.Specialized; //For NameValueCollection
 
 namespace ExternalBrowser
 {
-    public class DisHook
+    public class DisHook // Origianlly written by: https://github.com/KyeOnDiscord
     {
-        public static void SendDiscordWebhook(string URL, string profilepic, string username, string message)
+        public static void SendDiscordWebhook(string URL, string profile, string username, string message)
         {
             NameValueCollection discordValues = new NameValueCollection
             {
                 { "username", username },
-                { "avatar_url", profilepic },
+                { "avatar_url", profile },
                 { "content", message }
             };
             new WebClient().UploadValues(URL, discordValues);
